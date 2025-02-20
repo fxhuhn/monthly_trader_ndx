ec0d449448f1a610606e5ef28012371846a958b5
Markus <github@fxhuhn.de>
2025-02-20 09:50:53 +0100

    updated debug output

--- tools/strategy.py
+++ tools/strategy.py
@@ -6,28 +6,38 @@ def strategy(df, regime, last_ticker) -> pd.DataFrame:
 
     print(f"market        : {regime.breath} ")
     print(
-        f"market breath : {regime.breath_fast:.1f} > {regime.breath_slow:.1f} {regime.breath_fast>regime.breath_slow}"
+        f"market breath : {regime.breath_fast:.1f} > {regime.breath_slow:.1f} {regime.breath_fast > regime.breath_slow}"
     )
     print(
-        f"market regime : {regime.Close:.1f} > {regime.index_ma:.1f} {regime.Close>regime.index_ma}"
+        f"market regime : {regime.Close:.1f} > {regime.index_ma:.1f} {regime.Close > regime.index_ma}"
     )
 
     # ticker = []
 
     df["ROC"] = df["ROC_1"] + df["ROC_3"] + df["ROC_6"] + df["ROC_12"]
 
+    """
     print(
         df.loc[df["ROC"].nlargest(10).index][
             ["ROC", "ROC_1", "ROC_3", "ROC_6", "ROC_12"]
         ].sort_values("ROC", ascending=False)
     )
     print("---")
+    """
 
-    # ticker = list(set(df.index.unique()) - set(ticker))
+    ticker = []
     if regime.breath_fast > regime.breath_slow and regime.Close > regime.index_ma:
-        return df["ROC"].nlargest(MAX_TICKER).index
+        ticker = df["ROC"].nlargest(MAX_TICKER).index
     else:
         if len(last_ticker) > 0:
-            return list(set(last_ticker) & set(df["ROC"].nlargest(MAX_TICKER).index))
+            ticker = list(set(last_ticker) & set(df["ROC"].nlargest(MAX_TICKER).index))
 
-    return []
+    print("")
+    print(
+        df[df.index.isin(ticker)][
+            ["ROC", "ROC_1", "ROC_3", "ROC_6", "ROC_12"]
+        ].sort_values("ROC", ascending=False)
+    )
+    print("---")
+
+    return ticker
